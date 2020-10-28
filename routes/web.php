9769a9fe6e804632b181a526593b52c9c56c245d
Adam Reyner <reyneradam@gmail.com>
2020-10-28 12:26:34 +0700

    garapane tito

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+
+});
+
+Route::get('/login', function (){
+    return view('login');
+});
+
+Route::get('/blog', function (){
+    return view('blog');
 });
