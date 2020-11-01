1bb9cbd8cba32242492b2d82210f33023feb6fe5
Adam Reyner <reyneradam@gmail.com>
2020-11-01 16:57:57 +0700

    minor update

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -73,13 +73,13 @@ https://templatemo.com/tm-538-digital-trend
 
                     <div class="col-lg-12 col-12 mb-3 text-center">
 
-                      <h1 class="mb-4" data-aos="fade-up">Our Video Catalog</h1>
+                      <h1 class="mb-4 " data-aos="fade-up">Our Video Catalog</h1>
                     </div>
 
                     <div class="row" data-aos="fade-up">
                         <div class="col-12">
                             <div class="tm-categories-container mb-5">
-                                <h3 class="tm-text-primary tm-categories-text">Categories:</h3>
+                                <h3 class="tm-text-primary tm-categories-text video-category">Categories:</h3>
                                 <ul class="nav tm-category-list">
                                     <li class="nav-item tm-category-item"><a href="#" class="tm-category-link active">All</a></li>
                                     <li class="nav-item tm-category-item"><a href="#" class="tm-category-link">Drone Shots</a></li>
@@ -91,7 +91,7 @@ https://templatemo.com/tm-538-digital-trend
                         </div>
                     </div>
                     <div class="row">
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
@@ -100,7 +100,7 @@ https://templatemo.com/tm-538-digital-trend
                         </div>
                       </div>
 
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image01.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
@@ -109,7 +109,7 @@ https://templatemo.com/tm-538-digital-trend
                         </div>
                       </div>
 
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image02.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
@@ -117,7 +117,7 @@ https://templatemo.com/tm-538-digital-trend
                           <h3><a href="blog-detail.html">The Mistakes I Made As a Coding Beginner</a></h3>
                         </div>
                       </div>
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
@@ -126,7 +126,7 @@ https://templatemo.com/tm-538-digital-trend
                         </div>
                       </div>
 
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image01.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
@@ -135,7 +135,7 @@ https://templatemo.com/tm-538-digital-trend
                         </div>
                       </div>
 
-                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-4" data-aos="fade-up" data-aos-delay="200">
+                      <div class="blog-sidebar col-lg-4 col-md-6 col-sm-12 d-flex flex-column justify-content-left align-items-left mb-5" data-aos="fade-up" data-aos-delay="200">
                         <img src="images/blog/blog-sidebar-image02.jpg" class="img-fluid" alt="blog">
 
                         <div class="blog-info mt-2 mb-2">
